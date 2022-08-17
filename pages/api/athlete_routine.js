f259589f49e3f487209230c4b4d4d50888bf92b5
Chris <millarchris03@gmail.com>
2022-08-17 22:31:51 +0100

    Removing functions / mking pages responsive 4 user

--- pages/api/athlete_routine.js
+++ pages/api/athlete_routine.js
@@ -16,11 +16,12 @@ export default async function handler(req, res) {
   }
 }
 
-const sql = `SELECT athlete_element_routine.athlete_id, 
+const sql = `SELECT athlete.unique_identifier, athlete_element_routine.athlete_id, 
 athlete_element_routine.element_id, athlete_element_routine.routine_id, 
 athlete_element_routine.athlete_element_routine_id, routine.routine_name 
 FROM athlete_element_routine
 INNER JOIN routine ON athlete_element_routine.routine_id = routine.routine_id
+INNER JOIN athlete ON athlete_element_routine.athlete_id = athlete.athlete_id
  `;
 
 // Service function that grabs data from database - keeping the handler agnostic of what dataabse it is connected to [separation of concerns]
--- pages/workouts/hypertrophy/routineMenu.js
+++ pages/workouts/hypertrophy/routineMenu.js
@@ -12,32 +12,74 @@ import { lowerCaseFirstLetter } from "../../_app";
 //Constants for testing data pull
 const workoutTitle = "Hypertrophy";
 const workoutPathTitle = lowerCaseFirstLetter(workoutTitle);
-let passedAthlete = 1; //user.sub
 let passedElement = 3;
 //Constants for testing data pull
 
 export default function routineMenu() {
-  let routines = null;
-  routines = checkForRoutines(passedAthlete, passedElement);
+  const { user, isLoading } = useUser();
+  const [metrics, setMetrics] = useState(undefined);
 
-  return (
-    <>
-      <Meta title={workoutTitle} />
-      <Navbar title={workoutTitle} backPath={`/workouts/${workoutPathTitle}`} />
-      <PageLayout>
-        <div className="w-full flex justify-center">
-          <div className="mb-5 w-2/3 h-10 border-2 border-black rounded-md text-center">
-            Searchbar placeholder
-          </div>
-        </div>
-        {routines}
-        <Button
-          path={`/workouts/${workoutPathTitle}NewRoutine`}
-          label={"Create New Routine +"}
+  useEffect(() => {
+    (async function () {
+      try {
+        const response = await fetch("/api/athlete_routine");
+        const result = await response.json();
+
+        if (response.ok) {
+          setMetrics(result);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    })();
+  }, []);
+
+  let routines = null;
+
+  if (metrics !== undefined && user !== undefined) {
+    let routines = metrics
+      .filter(function (metric) {
+        return (
+          metric.unique_identifier === user.sub &&
+          metric.element_id === passedElement
+        );
+      })
+      .map(function (metric) {
+        return (
+          <button
+            key={metric.athlete_element_routine_id}
+            className=" w-full h-10 border-black border-2 flex "
+            onClick={function () {
+              Router.push(`/workouts/${workoutPathTitle}/routineDisplay`);
+              // -${metric.routine_id}
+            }}
+          >
+            {metric.routine_name}
+          </button>
+        );
+      });
+    return (
+      <>
+        <Meta title={workoutTitle} />
+        <Navbar
+          title={workoutTitle}
+          backPath={`/workouts/${workoutPathTitle}`}
         />
-      </PageLayout>
-    </>
-  );
+        <PageLayout>
+          <div className="w-full flex justify-center">
+            <div className="mb-5 w-2/3 h-10 border-2 border-black rounded-md text-center">
+              Searchbar placeholder
+            </div>
+          </div>
+          {routines}
+          <Button
+            path={`/workouts/${workoutPathTitle}NewRoutine`}
+            label={"Create New Routine +"}
+          />
+        </PageLayout>
+      </>
+    );
+  }
 }
 
 function checkForRoutines(passedAthlete, passedElement) {
