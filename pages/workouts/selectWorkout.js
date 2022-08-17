615d77b79176e9f81e39d427f169a30079419deb
Chris <millarchris03@gmail.com>
2022-08-17 19:08:05 +0100

    fixed responsivness on workout selector

--- pages/workouts/selectWorkout.js
+++ pages/workouts/selectWorkout.js
@@ -27,8 +27,8 @@ export default function selectWorkout() {
   }, []);
 
   let body = null;
-
-  if (metrics !== undefined) {
+  //NEED TO PUT REQUIRMENT FOR DEFINED USER OTHERIWSE FAILS ON REFRESH - SAME REASON NEED USEEFFECT FUNCTION
+  if (metrics !== undefined && user !== undefined) {
     let body = metrics
       .filter(function (metric) {
         return (
@@ -50,67 +50,65 @@ export default function selectWorkout() {
           </button>
         );
       });
-    return body;
-  }
-
-  return (
-    <>
-      <Meta title="Select Workout" />
-      <Navbar title="Workouts:" backPath={"/athlete"} />
-      <PageLayout>
-        <p className="text-center">
-          Please Select The Workout You Wish To Begin
-        </p>
-        <div>{body}</div>
-      </PageLayout>
-    </>
-  );
-}
-
-function checkWorkoutElements(athleteID0) {
-  const [metrics, setMetrics] = useState(undefined);
-
-  useEffect(() => {
-    (async function () {
-      try {
-        const response = await fetch("/api/athlete_elements");
-        const result = await response.json();
-
-        if (response.ok) {
-          setMetrics(result);
-        }
-      } catch (e) {
-        console.error(e);
-      }
-    })();
-  }, []);
-
-  if (metrics !== undefined) {
-    let body = metrics
-      .filter(function (metric) {
-        return (
-          metric.element_class_id === 3 &&
-          metric.unique_identifier == athleteID0
-        );
-      })
-      .map(function (metric) {
-        return (
-          <button
-            key={`${metric.element_id}-btn`}
-            className="h-20 w-full rounded-md border-black border-2 bg-slate-300 mb-2 place-content-center"
-            onClick={function () {
-              let path = lowerCaseFirstLetter(metric.element_name);
-              Router.push(`/workouts/${path}`);
-            }}
-          >
-            {" "}
-            {metric.element_name}
-          </button>
-        );
-      });
-    return body;
+    return (
+      <>
+        <Meta title="Select Workout" />
+        <Navbar title="Workouts:" backPath={"/athlete"} />
+        <PageLayout>
+          <p className="text-center">
+            Please Select The Workout You Wish To Begin
+          </p>
+          <div>{body}</div>
+        </PageLayout>
+      </>
+    );
   }
 }
+
+// function checkWorkoutElements(athleteID0) {
+//   const [metrics, setMetrics] = useState(undefined);
+
+//   useEffect(() => {
+//     (async function () {
+//       try {
+//         const response = await fetch("/api/athlete_elements");
+//         const result = await response.json();
+
+//         if (response.ok) {
+//           setMetrics(result);
+//         }
+//       } catch (e) {
+//         console.error(e);
+//       }
+//     })();
+//   }, []);
+
+//   if (metrics !== undefined) {
+//     let body = metrics
+//       .filter(function (metric) {
+//         return (
+//           metric.element_class_id === 3 &&
+//           metric.unique_identifier == athleteID0
+//         );
+//       })
+//       .map(function (metric) {
+//         return (
+//           <button
+//             key={`${metric.element_id}-btn`}
+//             className="h-20 w-full rounded-md border-black border-2 bg-slate-300 mb-2 place-content-center"
+//             onClick={function () {
+//               let path = lowerCaseFirstLetter(metric.element_name);
+//               Router.push(`/workouts/${path}`);
+//             }}
+//           >
+//             {" "}
+//             {metric.element_name}
+//           </button>
+//         );
+//       });
+//     return body;
+//   }
+// }
 
 // function lowerCaseFirstLetter(string) {
 //   return string.charAt(0).toLowerCase() + string.slice(1);
