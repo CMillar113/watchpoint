8193b2fbd423c37627168482d8740a0a18b50c33
Chris <millarchris03@gmail.com>
2022-08-29 09:02:00 +0100

    Week in review functionality began

---
+++ pages/api/user/weekReview.js
@@ -0,0 +1,59 @@
+/**
+ * Calls elements for users week in review
+ *
+ */
+
+import executeQuery from "../../../lib/db";
+
+// Controller function which is separated from the database logic and just returns data to frontend
+export default async function handler(req, res) {
+  const queryParams = req.query;
+  const { athlete0Id, date } = queryParams;
+  const today = date;
+  const backDate = date;
+
+  if (!athlete0Id) {
+    res.status(400).json({ message: "No ID specified" });
+  }
+  if (req.method === "GET") {
+    const metrics = await getHealthcare(athlete0Id);
+    console.log({ metrics });
+    res.status(200).json({
+      steps: getSteps(metrics),
+      bodyweight: getBodyWeight(metrics),
+      sleep: getSleep(metrics),
+    });
+  } else {
+    console.log(error);
+  }
+}
+
+// Service function that grabs data from database - keeping the handler agnostic of what dataabse it is connected to [separation of concerns]
+async function getHealthcare(athlete0Id, backDate, today) {
+  const sql = `
+  SELECT athlete.unique_identifier, healthcare_log.element_id, element.element_name, healthcare_log.date, healthcare_log.log_value  
+  FROM athlete 
+  INNER JOIN  healthcare_log on athlete.athlete_id = healthcare_log.athlete_id 
+  INNER JOIN  element on healthcare_log.element_id = element.element_id
+  WHERE athlete.unique_identifier = 'auth0|62fa26f0e578d985fe41dce9' AND healthcare_log.date BETWEEN ? AND ?
+  ORDER BY healthcare_log.date DESC 
+  `;
+  //IMPROVMENT - Split healthcare logs into ther own databasetables - be quicker searching and filtering when there are more users and data
+
+  const healthcare = await executeQuery({
+    query: sql,
+    values: [athlete0Id, backDate, today],
+  });
+  console.log(healthcare);
+  return healthcare;
+}
+
+function getSteps(metrics) {
+  return metrics.filter((log) => log.element_name === "Steps");
+}
+function getBodyWeight(metrics) {
+  return metrics.filter((log) => log.element_name === "Bodyweight");
+}
+function getSleep(metrics) {
+  return metrics.filter((log) => log.element_name === "Sleep");
+}
--- pages/athlete/reviewWeek.js
+++ pages/athlete/reviewWeek.js
@@ -1,11 +1,48 @@
 import Meta from "../../src/components/Meta";
 import Navbar from "../../src/components/NavBar";
 import PageLayout from "../../src/components/PageLayout";
-
 import Router from "next/router";
 import { useEffect, useState } from "react";
+import { useUser } from "@auth0/nextjs-auth0";
+
+const today = new Date().toISOString().substring(0, 10);
+const backDate = today - 7;
 
 export default function reviewWeek() {
+  const [calories, setCalories] = useState();
+  const { user } = useUser();
+  const [isLoading, setLoading] = useState(true);
+
+  //Only possible healthcare attributes - finite amount but if there were to be more this isnt the most scalable
+
+  const [bodyweightWeekArray, setbodyweightWeekArray] = useState();
+  const [stepsWeekArray, setStepsWeekArray] = useState();
+  const [sleepWeekArray, setSleepWeekArray] = useState();
+
+  useEffect(() => {
+    if (!user) return;
+    (async function () {
+      setLoading(true);
+      try {
+        const athlete0Id = user.sub;
+        const response = await fetch(
+          `/api/user/weekReview?athlete0Id=${athlete0Id}&date=${today}`
+        );
+        const result = await response.json();
+
+        console.log({ result });
+
+        if (response.ok) {
+          setLoggedBodyweight(result.bodyweight);
+        }
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [user]);
+
   return (
     <>
       <Meta title="Athlete Week Review" />
@@ -18,6 +55,9 @@ export default function reviewWeek() {
           id="All"
           className="w-full h-screen flex-col  border-2 border-black "
         >
+          <p className=" justify-evenly flex">
+            From: {backDate} To: {today}
+          </p>
           <div
             id="Day"
             className="h-1/6 w-full border-black border-2 flex"
